71dbdfc1eebce59c6a633e276d9d0867571605a5
Fabio Valeriano <valerianofabio@gmail.com>
2026-08-12 10:20:36 +0200

    Use Google credentials from environment

--- lib/google.ts
+++ lib/google.ts
@@ -3,18 +3,33 @@ import { JWT } from "google-auth-library";
 import fs from "fs";
 import path from "path";
 
-const credentialsPath = path.join(
-  process.cwd(),
-  "lezioni-allievi-4b056182017b.json"
-);
+let clientEmail: string;
+let privateKey: string;
 
-const credentials = JSON.parse(
-  fs.readFileSync(credentialsPath, "utf8")
-);
+// Se sono presenti le variabili d'ambiente,
+// usiamo quelle (Vercel).
+if (process.env.GOOGLE_CLIENT_EMAIL && process.env.GOOGLE_PRIVATE_KEY) {
+  clientEmail = process.env.GOOGLE_CLIENT_EMAIL;
+
+  privateKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n");
+} else {
+  // In locale continuiamo a usare il file JSON.
+  const credentialsPath = path.join(
+    process.cwd(),
+    "lezioni-allievi-4b056182017b.json"
+  );
+
+  const credentials = JSON.parse(
+    fs.readFileSync(credentialsPath, "utf8")
+  );
+
+  clientEmail = credentials.client_email;
+  privateKey = credentials.private_key;
+}
 
 const auth = new JWT({
-  email: credentials.client_email,
-  key: credentials.private_key,
+  email: clientEmail,
+  key: privateKey,
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
